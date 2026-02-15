ad534422982421fa5132b97c6d8ccf7116e3992c
azozsindi <azozsindi23@gmail.com>
2026-02-15 15:22:14 +0300

    Create supabaseClient.ts

---
+++ supabaseClient.ts
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://sdxtpldpxewvkjwmlkcz.supabase.co'; 
+const supabaseAnonKey = 'sb_publishable_c0pLNtJrtegGwEFCvgAOGQ_Z9ag411h';
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
