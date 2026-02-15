a1d930e3d2295854355a92b6abd5336d4f570db7
azozsindi <azozsindi23@gmail.com>
2026-02-15 15:44:38 +0300

    Update supabaseClient.ts

--- supabaseClient.ts
+++ supabaseClient.ts
@@ -1,5 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
+// بيانات مشروعك (جاهزة للعمل)
 const supabaseUrl = 'https://sdxtpldpxewvkjwmlkcz.supabase.co'; 
 const supabaseAnonKey = 'sb_publishable_c0pLNtJrtegGwEFCvgAOGQ_Z9ag411h';
 
